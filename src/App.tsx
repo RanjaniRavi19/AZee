4e6c27e02883594f7d3d7d367d6f3e3cf73c2b88
RanjaniRavi19 <107185215+RanjaniRavi19@users.noreply.github.com>
2023-04-24 15:31:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello this is Naga
+          Hello this is pyatham
         </p>
       </header>
     </div>
